dacb318c467a267dd25f322aa39430c625b2fdf7
Brendan Duke <dukebw@mcmaster.ca>
2017-10-14 20:10:36 -0400

    Evaluation using gym, and plotting of return.

--- week2/behaviour_cloning.py
+++ week2/behaviour_cloning.py
@@ -3,8 +3,10 @@ import argparse
 import pickle
 import random
 
+import gym
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
 import torch
 
 
@@ -25,91 +27,158 @@ def _get_rollout(obs, actions, batch_i, batch_indices, batch_size):
 
     return obs, actions
 
-def behaviour_cloning(bhvr_clone_gt_file, batch_size, num_epochs):
+
+def _evaluate(plt_mean_returns, env, model):
+    """Evaluate mean return of model and store in plt_mean_returns."""
+    model.eval()
+    returns = []
+    obs = env.reset()
+    for _ in range(10):
+        obs = env.reset()
+        done = False
+        total_r = 0.0
+        steps = 0
+        while not done:
+            obs = torch.from_numpy(obs.astype(np.float32))
+            obs = torch.autograd.Variable(obs, volatile=True)
+            action = model(obs.cuda())
+            obs, r, done, _ = env.step(action.cpu().data.numpy())
+            total_r += r
+            steps += 1
+            if steps >= env.spec.timestep_limit:
+                break
+
+        returns.append(total_r)
+
+    plt_mean_returns.append(np.mean(returns))
+
+
+def _train_single_epoch(plt_train_mean_losses,
+                        boxs_loop,
+                        batch_indices,
+                        batch_size):
+    """Train model for one epoch."""
+    random.shuffle(batch_indices)
+    boxs_loop['model'].train()
+
+    losses = []
+    for batch_i in range(boxs_loop['data']['obs'].shape[0]//batch_size):
+        obs, actions = _get_rollout(boxs_loop['data']['obs'],
+                                    boxs_loop['data']['action'],
+                                    batch_i,
+                                    batch_indices,
+                                    batch_size)
+
+        obs = torch.autograd.Variable(obs, requires_grad=True).cuda()
+        actions = torch.autograd.Variable(actions).cuda(async=True)
+
+        pred = boxs_loop['model'](obs)
+
+        mse_loss = boxs_loop['criticism'](pred, actions)
+        losses.append(mse_loss.data[0])
+
+        boxs_loop['optimizer'].zero_grad()
+        mse_loss.backward()
+        boxs_loop['optimizer'].step()
+
+    plt_train_mean_losses.append(np.mean(losses))
+
+
+def _get_model(expert_data):
+    """MLP with four layers of 32 hidden units each."""
+    return torch.nn.Sequential(
+        torch.nn.Linear(
+            in_features=expert_data['observations'].shape[-1],
+            out_features=32,
+            bias=True),
+        torch.nn.ReLU(),
+        torch.nn.Linear(
+            in_features=32,
+            out_features=32,
+            bias=True),
+        torch.nn.ReLU(),
+        torch.nn.Linear(
+            in_features=32,
+            out_features=32,
+            bias=True),
+        torch.nn.ReLU(),
+        torch.nn.Linear(
+            in_features=32,
+            out_features=expert_data['actions'].shape[-1],
+            bias=True),
+        )
+
+
+def behaviour_cloning(bhvr_clone_gt_file, batch_size, num_epochs, envname):
     """Imitation learning from saved (obs, actions) pairs from an expert
     policy.
     """
+    torch.backends.cudnn.benchmark = True
+
+    env = gym.make(envname)
+
     with open(bhvr_clone_gt_file, 'rb') as f:
         expert_data = pickle.load(f)
 
-    num_train = int(0.7*expert_data['observations'].shape[0])
-    obs_train = expert_data['observations'][:num_train, ...]
-    obs_val = expert_data['observations'][num_train:, ...]
+    obs_train = expert_data['observations']
+    actions_train = expert_data['actions']
 
-    actions_train = expert_data['actions'][:num_train, ...]
-    actions_val = expert_data['actions'][num_train:, ...]
+    model = _get_model(expert_data).cuda()
 
-    model = torch.nn.Linear(
-        in_features=expert_data['observations'].shape[-1],
-        out_features=expert_data['actions'].shape[-1],
-        bias=True)
+    criticism = torch.nn.MSELoss().cuda()
 
-    criticism = torch.nn.MSELoss()
+    biases = [p for name, p in model.named_parameters() if 'bias' in name]
+    weights = [p for name, p in model.named_parameters() if 'bias' not in name]
+    optimizer = torch.optim.Adam([{'params': biases, 'weight_decay': 0},
+                                  {'params': weights, 'weight_decay': 1e-4}],
+                                 lr=1e-3)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+    boxs_loop = {'data': {'obs': obs_train, 'action': actions_train},
+                 'model': model,
+                 'criticism': criticism,
+                 'optimizer': optimizer}
 
     plt_train_mean_losses = []
-    plt_val_mean_losses = []
+    plt_mean_returns = []
     for _ in range(num_epochs):
-        model.train()
         batch_indices = list(range(obs_train.shape[0]))
-        random.shuffle(batch_indices)
 
-        losses = []
-        for batch_i in range(obs_train.shape[0]//batch_size):
-            obs, actions = _get_rollout(obs_train,
-                                        actions_train,
-                                        batch_i,
-                                        batch_indices,
-                                        batch_size)
+        _train_single_epoch(plt_train_mean_losses,
+                            boxs_loop,
+                            batch_indices,
+                            batch_size)
 
-            obs = torch.autograd.Variable(obs, requires_grad=True)
-            actions = torch.autograd.Variable(actions)
-
-            pred = model(obs)
-
-            mse_loss = criticism(pred, actions)
-            losses.append(mse_loss.data[0])
-
-            optimizer.zero_grad()
-            mse_loss.backward()
-            optimizer.step()
-
-        plt_train_mean_losses.append(np.mean(losses))
-
-        model.eval()
-        losses = []
-        batch_indices = list(range(obs_val.shape[0]))
-        for batch_i in range(obs_val.shape[0]//batch_size):
-            obs, actions = _get_rollout(obs_val,
-                                        actions_val,
-                                        batch_i,
-                                        batch_indices,
-                                        batch_size)
-
-            obs = torch.autograd.Variable(obs, volatile=True)
-            actions = torch.autograd.Variable(actions, volatile=True)
-
-            pred = model(obs)
-
-            mse_loss = criticism(pred, actions)
-            losses.append(mse_loss.data[0])
-
-        plt_val_mean_losses.append(np.mean(losses))
+        _evaluate(plt_mean_returns, env, model)
 
     epochs = list(range(len(plt_train_mean_losses)))
+
+    plt.figure(1)
+    ax = plt.subplot(211)
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Training loss')
+
     plt.plot(epochs, plt_train_mean_losses)
-    plt.plot(epochs, plt_val_mean_losses)
+
+    ax = plt.subplot(212)
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Return')
+
+    plt.plot(epochs, plt_mean_returns)
+
     plt.show()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--bhvr-clone-gt-file', type=str, default=None)
+    parser.add_argument('--envname', type=str, default=None)
     parser.add_argument('--batch-size', type=int, default=None)
     parser.add_argument('--num-epochs', type=int, default=None)
     args = parser.parse_args()
 
-    behaviour_cloning(args.bhvr_clone_gt_file,
-                      args.batch_size,
-                      args.num_epochs)
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.1)
+    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)):
+        behaviour_cloning(args.bhvr_clone_gt_file,
+                          args.batch_size,
+                          args.num_epochs,
+                          args.envname)
